c83ce1dbc039272953a876ac10b5354fd5eba02e
mehmetsungur <mehmetsungur90@gmail.com>
2023-08-30 23:13:46 +0300

    update

--- src/test/java/Day01/Test04_BeforeAfterAll.java
+++ src/test/java/Day01/Test04_BeforeAfterAll.java
@@ -15,7 +15,19 @@ public class Test04_BeforeAfterAll {
     }
 
     @Test
+    @Order(2)
     void testSplitWithArrays(TestInfo info){
+        String str="JUnit Working";
+        String[] realValue = str.split(" ");
+        String[] waitValue = {"JUnit", "Working"};
+
+        System.out.println(info.getDisplayName() + " working!");
+        Assertions.assertArrayEquals(waitValue,realValue);
+    }
+
+    @Test
+    @Order(1)
+    void testSplitWithArrays2(TestInfo info){
         String str="JUnit Working";
         String[] realValue = str.split(" ");
         String[] waitValue = {"JUnit", "Working"};
