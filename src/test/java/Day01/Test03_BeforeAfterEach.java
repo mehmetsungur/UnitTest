fb1e4e6fe5b7603a2af8edccf0e11475bf2a6f78
mehmetsungur <mehmetsungur90@gmail.com>
2023-08-30 22:52:29 +0300

    update

--- src/test/java/Day01/Test03_BeforeAfterEach.java
+++ src/test/java/Day01/Test03_BeforeAfterEach.java
@@ -1,6 +1,8 @@
 package Day01;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 
 public class Test03_BeforeAfterEach {
@@ -14,5 +16,22 @@ public class Test03_BeforeAfterEach {
     @AfterEach
     void afterEach(){
         str=null;
+        System.out.println("********** afterEach working!");
+    }
+
+    @Test
+    void testSplit(){
+        String[] realValue = str.split(" ");
+        String[] waitValue = {"JUnit", "Working"};
+
+        Assertions.assertArrayEquals(waitValue,realValue);
+    }
+
+    @Test
+    void testLength(){
+        String[] realValue = str.split(" ");
+        String[] waitValue = {"JUnit", "Working"};
+
+        Assertions.assertArrayEquals(waitValue,realValue);
     }
 }
