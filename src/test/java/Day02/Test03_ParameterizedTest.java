cfda402b1d86626baa595574a2adea2a8c2f6d61
mehmetsungur <mehmetsungur90@gmail.com>
2023-09-01 16:54:31 +0300

    update

--- src/test/java/Day02/Test03_ParameterizedTest.java
+++ src/test/java/Day02/Test03_ParameterizedTest.java
@@ -2,7 +2,10 @@ package Day02;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Test03_ParameterizedTest {
@@ -18,5 +21,22 @@ public class Test03_ParameterizedTest {
     @ValueSource(strings = {"a","b","c","d"})
     void testLength2(String str){
         assertTrue(str.length()>0);
+    }
+
+    @Test
+    void testUpperCase(){
+        String str1="a";
+        String str2="b";
+        String str3="c";
+
+        assertEquals("a",str1.toUpperCase());
+        assertEquals("b",str2.toUpperCase());
+        assertEquals("c",str3.toUpperCase());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"a,A", "b,B","c,C"})
+    void testUpperCase2(String str1, String str2){
+        assertEquals(str2,str1.toUpperCase());
     }
 }
