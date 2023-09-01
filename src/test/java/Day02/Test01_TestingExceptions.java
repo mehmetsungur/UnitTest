36e91b24e9adf66b563dc48a7a7914b5f436f41e
mehmetsungur <mehmetsungur90@gmail.com>
2023-09-01 16:17:05 +0300

    update

--- src/test/java/Day02/Test01_TestingExceptions.java
+++ src/test/java/Day02/Test01_TestingExceptions.java
@@ -1,7 +1,6 @@
 package Day02;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Test01_TestingExceptions {
@@ -12,6 +11,32 @@ public class Test01_TestingExceptions {
 
         assertThrows(NullPointerException.class,()->{
            str.length();
+        });
+    }
+
+    @Test
+    void testExceptions2(){
+        int sayi1=6;
+        int sayi2=0;
+
+        // System.out.println(sayi1/sayi2);
+        assertThrows(ArithmeticException.class,()->{
+            System.out.println(sayi1/sayi2);
+        });
+    }
+
+    private int divide(int x, int y){
+        return x/y;
+    }
+
+    @Test
+    void testExceptions3(){
+        int sayi1=6;
+        int sayi2=0;
+
+        // System.out.println(sayi1/sayi2);
+        assertThrows(ArithmeticException.class,()->{
+            divide(sayi1,sayi2);
         });
     }
 }
