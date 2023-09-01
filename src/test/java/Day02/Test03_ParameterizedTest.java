c136756693fc625072218e2ec8c98160437a31f6
mehmetsungur <mehmetsungur90@gmail.com>
2023-09-01 16:46:04 +0300

    update

--- src/test/java/Day02/Test03_ParameterizedTest.java
+++ src/test/java/Day02/Test03_ParameterizedTest.java
@@ -1,6 +1,9 @@
 package Day02;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Test03_ParameterizedTest {
@@ -10,5 +13,11 @@ public class Test03_ParameterizedTest {
         assertTrue("b".length()>0);
         assertTrue("c".length()>0);
         assertTrue("d".length()>0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a","b","c","d"})
+    void testLength2(String str){
+        assertTrue(str.length()>0);
     }
 }
