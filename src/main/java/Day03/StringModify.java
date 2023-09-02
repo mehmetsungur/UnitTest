fd4e25dabeecbe91d9bcfc9e63c99fae21f4fc2a
mehmetsungur <mehmetsungur90@gmail.com>
2023-09-02 12:14:57 +0300

    update

--- src/main/java/Day03/StringModify.java
+++ src/main/java/Day03/StringModify.java
@@ -2,6 +2,10 @@ package Day03;
 
 public class StringModify {
     public String deleteIfAIsInTwoPosition(String str){
+        if(str.length()<=2){ //AA -> "", AB -> B
+            str.replaceAll("A","");
+        }
+
         String firstTwoChars = str.substring(0,2); //AABCD->AA
         String afterTwoChars = str.substring(2);
 
