2499d727abe17a2ddbc41925dd944832bdeafc63
mehmetsungur <mehmetsungur90@gmail.com>
2023-09-02 12:10:36 +0300

    update

--- src/test/java/Day03/StringModifyTest.java
+++ src/test/java/Day03/StringModifyTest.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringModifyTest {
-    StringModify object = new StringModify();
+    StringModify object=new StringModify();
 
     @ParameterizedTest
-    @CsvSource(value = {"BC,ABC","B,AB"})
+    @CsvSource(value = {"BC,ABC","B,AB","BABCD,ABABCD","'',AA","BCD,BACD"})
     void deleteIfAIsInTwoPositionTest(String expected, String param){
-        assertEquals(expected.object.deleteIfAIsInTwoPosition(param));
+        assertEquals(expected,object.deleteIfAIsInTwoPosition(param));
     }
 }
